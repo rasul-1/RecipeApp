a546d75e2c288972e8cebdc0b917907d0101e8fd
rasul-1 <muxtorovmuhammadrasul62@gmail.com>
2024-11-12 19:01:17 +0500

    I added some pictures

---
+++ recipe_app/lib/onboard/board2.dart
@@ -0,0 +1,3 @@
+class OnBoard2 {
+  
+}
---
+++ recipe_app/lib/onboard/onboard.dart
@@ -0,0 +1,10 @@
+class Board {
+  String title;
+  String image;
+  String info;
+
+  Board({required this.title,required this.image,required this.info});
+}
+
+
+
